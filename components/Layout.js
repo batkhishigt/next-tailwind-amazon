38c3aa0dace631aabf79f6503e75db070bde8553
batkhishigt <batkhishigt.u@gmail.com>
2022-06-24 13:10:15 +0800

    add header and footer

---
+++ components/Layout.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import Head from 'next/head'
+import Link from 'next/link'
+
+export default function Layout({ title, children }) {
+    return (
+        <>
+            <Head>
+                <title>{title ? title + '- Amazon ' : "Amazon"}</title>
+                <meta name="description" content="Ecmmerce wibsite" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <div className='flex min-h-screen flex-col justify-between'>
+                <header>
+                    <nav className='flex h-12 justify-between items-center px-4 shadow-md'>
+                        <Link href="/">
+                            <a className='text-lg font-bold'>Amazon</a>
+                        </Link>
+                        <div>
+                            <Link href="/cart">
+                                <a className='p-2'>
+                                    Cart
+                                </a>
+                            </Link>
+                            <Link href="/login">
+                                <a className='p-2'>
+                                    Login
+                                </a>
+                            </Link>
+                        </div>
+                    </nav>
+                </header>
+                <main className='container m-auto mt-4 px-4 '>
+                    {children}
+                </main>
+                <footer className='flex h-10 justify-center items-center shadow-inner'>
+                    <p>Coryright 2022 Amozan</p>
+                </footer>
+            </div>
+
+        </>
+    )
+}
