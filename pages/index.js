d38d61cd0e40c08beab3a331aa8ff4f7dabea362
batkhishigt <batkhishigt.u@gmail.com>
2022-06-24 15:03:49 +0800

    add to product item

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,17 @@
 import Layout from "../components/Layout";
+import ProductItem from "../components/ProductItem";
+import data from "../utils/data";
 
 
 export default function Home() {
   return (
     <Layout title={'Home Page'}>
-      home page
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
+        {data.products.map(p => {
+
+          return <ProductItem product={p} key={p.slug} ></ProductItem>
+        })}
+      </div>
     </Layout >
   )
 }
---
+++ utils/data.js
@@ -0,0 +1,79 @@
+const data = {
+    products: [
+        {
+            name: "Free shirt",
+            slug: "free-shirts",
+            category: 'Shirts',
+            image: 'images/shirts1.jpg',
+            price: 70,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 8,
+            countInStock: 20,
+            description: "A popular shirt"
+        },
+        {
+            name: "Fit shirt",
+            slug: "fit-shirts",
+            category: 'Shirts',
+            image: 'images/shirts2.jpg',
+            price: 60,
+            brand: 'Nike',
+            rating: 4.0,
+            numReviews: 8,
+            countInStock: 10,
+            description: "A popular shirt"
+        },
+        {
+            name: "Slim shirt",
+            slug: "slim-shirts",
+            category: 'Shirts',
+            image: 'images/shirts3.jpg',
+            price: 80,
+            brand: 'Adidas',
+            rating: 3.0,
+            numReviews: 8,
+            countInStock: 25,
+            description: "A popular shirt"
+        }, {
+            name: "Golf shirt",
+            slug: "glof-shirts",
+            category: 'Shirts',
+            image: 'images/shirts4.jpg',
+            price: 80,
+            brand: 'Adidas',
+            rating: 3.0,
+            numReviews: 12,
+            countInStock: 25,
+            description: "A popular shirt"
+        },
+        {
+            name: "Golf shirt",
+            slug: "glof-shirts",
+            category: 'Shirts',
+            image: 'images/shirts5.jpg',
+            price: 80,
+            brand: 'Adidas',
+            rating: 3.0,
+            numReviews: 12,
+            countInStock: 25,
+            description: "A popular shirt"
+        },
+        {
+            name: "Golf shirt",
+            slug: "glof-shirts",
+            category: 'Shirts',
+            image: 'images/shirts6.jpg',
+            price: 80,
+            brand: 'Adidas',
+            rating: 3.0,
+            numReviews: 12,
+            countInStock: 25,
+            description: "A popular shirt"
+        },
+
+
+    ]
+};
+
+export default data;
